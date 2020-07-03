e299d9a8bd5913b8078d4ac47d7c96814eba651c
Shonuvik <34462179+Shonuvik@users.noreply.github.com>
2020-07-03 16:53:48 -0300

    Excluded auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,17 @@
 const { verifyJwt } = require("../helpers/jwt");
 
 
-
-
-
-
 const checkJwt = (req, res, next) =>{
+
+    // acabar com a verificação do JWT nessas rotas auth/sign-in
+    // auth/sign-up
+
+    //aliás: estou renomeado a variavel url para path
+    const {url: path} = req;
+    
+    const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+    const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+    if(isExcluded) return next();
 
    let token = req.headers['authorization'];
    token = token ? token.slice(7,  token.lenght) : null;
